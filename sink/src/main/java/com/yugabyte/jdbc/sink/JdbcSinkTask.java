a1ce387ad9bb3325dba494284a25dbd755c453b4
williamyadi <william.yadi@tokopedia.com>
2024-04-30 18:44:44 +0700

    chore(dep): adjust latest depedencies - postgresql - jackson

--- sink/src/main/java/com/yugabyte/jdbc/sink/JdbcSinkTask.java
+++ sink/src/main/java/com/yugabyte/jdbc/sink/JdbcSinkTask.java
@@ -33,111 +33,124 @@ import org.slf4j.LoggerFactory;
 public class JdbcSinkTask extends SinkTask {
   private static final Logger log = LoggerFactory.getLogger(JdbcSinkTask.class);
 
-  DatabaseDialect dialect;
-  JdbcSinkConfig config;
-  JdbcDbWriter writer;
-  int remainingRetries;
+    DatabaseDialect dialect;
+    JdbcSinkConfig config;
+    JdbcDbWriter writer;
+    int remainingRetries;
 
-  @Override
-  public void start(final Map<String, String> props) {
-    log.info("Starting JDBC Sink task");
-    config = new JdbcSinkConfig(props);
-    initWriter();
-    remainingRetries = config.maxRetries;
-  }
+    @Override
+    public void start(final Map<String, String> props) {
+      log.info("Starting JDBC Sink task");
+      try {
+        Class.forName("org.postgresql.Driver");
+        log.info("PostgreSQL JDBC Driver loaded successfully.");
+      } catch (ClassNotFoundException e) {
+        log.error("PostgreSQL JDBC Driver not found in class path", e);
+        throw new ConnectException("PostgreSQL JDBC Driver not found", e);
+      }
 
-  void initWriter() {
-    dialect = DatabaseDialects.create("PostgreSqlDatabaseDialect", config);
-    final DbStructure dbStructure = new DbStructure(dialect);
-    log.info("Initializing writer using SQL dialect: {}", dialect.getClass().getSimpleName());
-    writer = new JdbcDbWriter(config, dialect, dbStructure);
-  }
-
-  @Override
-  public void put(Collection<SinkRecord> records) {
-    if (records.isEmpty()) {
-      return;
+      config = new JdbcSinkConfig(props);
+      initWriter();
+      remainingRetries = config.maxRetries;
     }
-    final SinkRecord first = records.iterator().next();
-    final int recordsCount = records.size();
-    log.debug(
-        "Received {} records. First record kafka coordinates:({}-{}-{}). Writing them to the "
-            + "database...",
-        recordsCount,
-        first.topic(),
-        first.kafkaPartition(),
-        first.kafkaOffset());
-    try {
-      long start = System.currentTimeMillis();
-      writer.write(records);
-      long end = System.currentTimeMillis();
-      log.info(
-          "The total time taken to write the batch({}-{}-{}) is : {} ms",
+
+    void initWriter() {
+      dialect = DatabaseDialects.create("PostgreSqlDatabaseDialect", config);
+      if (dialect == null) {
+        log.error("Failed to initialize the PostgreSQL database dialect.");
+        throw new ConnectException("Database dialect initialization failed.");
+      }
+      final DbStructure dbStructure = new DbStructure(dialect);
+      log.info("Initializing writer using SQL dialect: {}", dialect.getClass().getSimpleName());
+      writer = new JdbcDbWriter(config, dialect, dbStructure);
+    }
+
+    @Override
+    public void put(Collection<SinkRecord> records) {
+      if (records.isEmpty()) {
+        return;
+      }
+      final SinkRecord first = records.iterator().next();
+      final int recordsCount = records.size();
+      log.debug(
+          "Received {} records. First record kafka coordinates:({}-{}-{}). Writing them to the "
+              + "database...",
+          recordsCount,
           first.topic(),
           first.kafkaPartition(),
-          first.kafkaOffset(),
-          (end - start));
-
-    } catch (SQLException sqle) {
-      log.warn(
-          "Write of {} records failed, remainingRetries={}",
-          records.size(),
-          remainingRetries,
-          sqle);
-      String sqleAllMessages = "Exception chain:" + System.lineSeparator();
-      int totalExceptions = 0;
-      for (Throwable e : sqle) {
-        sqleAllMessages += e + System.lineSeparator();
-        totalExceptions++;
-      }
-      SQLException sqlAllMessagesException = new SQLException(sqleAllMessages);
-      sqlAllMessagesException.setNextException(sqle);
-      if (remainingRetries == 0) {
-        log.error(
-            "Failing task after exhausting retries; "
-                + "encountered {} exceptions on last write attempt. "
-                + "For complete details on each exception, please enable DEBUG logging.",
-            totalExceptions);
-        int exceptionCount = 1;
-        for (Throwable e : sqle) {
-          log.debug("Exception {}:", exceptionCount++, e);
-        }
-        throw new ConnectException(sqlAllMessagesException);
-      } else {
-        writer.closeQuietly();
-        initWriter();
-        remainingRetries--;
-        context.timeout(config.retryBackoffMs);
-        throw new RetriableException(sqlAllMessagesException);
-      }
-    }
-    remainingRetries = config.maxRetries;
-  }
-
-  @Override
-  public void flush(Map<TopicPartition, OffsetAndMetadata> map) {
-    // Not necessary
-  }
-
-  public void stop() {
-    log.info("Stopping task");
-    try {
-      writer.closeQuietly();
-    } finally {
+          first.kafkaOffset());
       try {
-        if (dialect != null) {
-          dialect.close();
+        long start = System.currentTimeMillis();
+        writer.write(records);
+        long end = System.currentTimeMillis();
+        log.info(
+            "The total time taken to write the batch({}-{}-{}) is : {} ms",
+            first.topic(),
+            first.kafkaPartition(),
+            first.kafkaOffset(),
+            (end - start));
+
+      } catch (SQLException sqle) {
+        log.warn(
+            "Write of {} records failed, remainingRetries={}",
+            records.size(),
+            remainingRetries,
+            sqle);
+        String sqleAllMessages = "Exception chain:" + System.lineSeparator();
+        int totalExceptions = 0;
+        for (Throwable e : sqle) {
+          sqleAllMessages += e + System.lineSeparator();
+          totalExceptions++;
         }
-      } catch (Throwable t) {
-        log.warn("Error while closing the {} dialect: ", dialect.name(), t);
+        SQLException sqlAllMessagesException = new SQLException(sqleAllMessages);
+        sqlAllMessagesException.setNextException(sqle);
+        if (remainingRetries == 0) {
+          log.error(
+              "Failing task after exhausting retries; "
+                  + "encountered {} exceptions on last write attempt. "
+                  + "For complete details on each exception, please enable DEBUG logging.",
+              totalExceptions);
+          int exceptionCount = 1;
+          for (Throwable e : sqle) {
+            log.debug("Exception {}:", exceptionCount++, e);
+          }
+          throw new ConnectException(sqlAllMessagesException);
+        } else {
+          writer.closeQuietly();
+          initWriter();
+          remainingRetries--;
+          context.timeout(config.retryBackoffMs);
+          throw new RetriableException(sqlAllMessagesException);
+        }
+      }
+      remainingRetries = config.maxRetries;
+    }
+
+    @Override
+    public void flush(Map<TopicPartition, OffsetAndMetadata> map) {
+      // Not necessary
+    }
+
+    public void stop() {
+      log.info("Stopping task");
+      try {
+        writer.closeQuietly();
       } finally {
-        dialect = null;
+        try {
+          if (dialect != null) {
+            dialect.close();
+          }
+        } catch (Throwable t) {
+          log.warn("Error while closing the {} dialect: ", dialect.name(), t);
+        } finally {
+          dialect = null;
+        }
       }
     }
-  }
 
-  @Override
-  public String version() {
-    return Version.getVersion();
+    @Override
+    public String version() {
+      return Version.getVersion();
+    }
   }
-}
+  
